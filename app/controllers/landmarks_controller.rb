9b6e0d1efb68ec86fb3459f809538367fca7377d
maryfarner18 <maryfarner@gmail.com>
2019-10-09 12:02:11 -0400

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,59 @@
 class LandmarksController < ApplicationController
-  # add controller methods
+  
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  #FILL FORM for NEW landmark
+  get '/landmarks/new' do
+    @figures = Figure.all
+
+    erb :'/landmarks/new'
+  end
+
+  #CREATE new landmark
+  post '/landmarks' do
+    @landmark = Landmark.create(params["landmark"])
+
+    if params["figure_name"] != ""
+      @landmark.figure_id = Figure.create(name: params["figure_name"]).id
+      @landmark.save
+    end
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  #FILL FORM for UPdate
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+    @figures = Figure.all
+    
+    erb :'/landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params["landmark"])
+
+    #if we made a new figure
+    if params["figure_name"] != ""
+      @landmark.figure_id = Figure.create(name: params["figure_name"]).id
+      @landmark.save
+    end
+
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  delete '/landmarks/:id' do
+    Landmark.destroy(params[:id])
+    redirect '/landmarks'
+  end
+
 end
